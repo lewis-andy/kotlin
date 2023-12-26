328559d052a13c2fe1494c8764ec5d8672c3c709
lewis-andy <lewisandanje3@gmail.com>
2023-12-26 22:38:41 +0300

    week 1

---
+++ start/first.kt
@@ -0,0 +1,11 @@
+fun main() {
+    //first code
+    var name = "Lewis andanje"
+    val birth_date = 31
+    val sum = 30
+    println(name)
+    println(birth_date)
+    println(sum)
+    println("Hello world")
+    println("This is my first kotlin statement to be printed on the terminal ")
+}
